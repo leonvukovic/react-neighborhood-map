e29d3ccfdce44a52360177771b8f3360b158848a
Leon Vukovic <vukovic.leon@gmail.com>
2018-09-15 17:50:26 +0200

    feat: Markers from Foursquare api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
-    this.loadMap();
+    this.getVenues()
+  }
+
+  // Get Forsquare data
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "RBCZ3PMP2LKASH0HSVROBYWNQIIL2W0KDUG5U3EM3VGJWOPB",
+      client_secret: "BKBSL1SEYCQJIC5D5HLORDHGO1XI51MHDSWZLQCWKKSFKUT1",
+      query: "pizza",
+      near: "Varaždin",
+      v: "20180409"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          // Data in state
+          venues: response.data.response.groups[0].items
+        }, this.loadMap())
+      })
+      .catch(error => {
+        alert("ERROR!! " + error)
+      })
   }
 
   loadMap = () => {
@@ -14,9 +42,17 @@ class App extends Component {
 
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    }); 
+      center: {lat: 46.305746, lng: 16.336607},
+      zoom: 13
+    });
+
+    this.state.venues.map(markers => {
+      const marker = new window.google.maps.Marker({
+        position: {lat: markers.venue.location.lat, lng: markers.venue.location.lng},
+        map: map,
+        title: markers.venue.name
+      });
+    })
   }
 
   render() {
@@ -27,8 +63,6 @@ class App extends Component {
     );
   }
 }
-
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
 
 function scriptLoad(url) {
   const index = window.document.getElementsByTagName("script")[0]
