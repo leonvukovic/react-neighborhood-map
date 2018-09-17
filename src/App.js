a1015a640fafb0e431d8c18ea114df8c3ac3022e
Leon Vukovic <vukovic.leon@gmail.com>
2018-09-17 09:24:21 +0200

    feat: Marker refresh in progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import escapeRegExp from 'escape-string-regexp';
+
+var map;
 
 class App extends Component {
 
   state = {
-    venues: []
+    venues: [],
+    markers: [],
+    query: ''
   }
 
   componentDidMount() {
     this.getVenues()
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+    this.checkQuery();
+    this.setMarkers();
+  }
+
+  updateMarkers = (markers) => {
+    console.log(markers);
+    this.setState({ markers: markers })
   }
 
   // Get Forsquare data
@@ -41,23 +57,46 @@ class App extends Component {
   }
 
   initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
+    map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 46.305746, lng: 16.336607},
       zoom: 13
     });
+    this.checkQuery();
+    this.setMarkers();
+  }
 
-    this.state.venues.map(markers => {
+  setMarkers = () => {
+    this.state.markers.map(markers => {
       const marker = new window.google.maps.Marker({
         position: {lat: markers.venue.location.lat, lng: markers.venue.location.lng},
         map: map,
         title: markers.venue.name
       });
+      marker.setMap(map);
     })
+  }
+
+  checkQuery = () => {
+    // Filter the markers based on the query
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      this.updateMarkers(this.state.venues.filter((venues) => match.test(venues.venue.name)))
+    } else {
+      this.updateMarkers(this.state.venues);
+    }
   }
 
   render() {
     return (
       <main>
+        <input
+          aria-label="Search places"
+          className="App-search-places"
+          type="text"
+          placeholder="Search places"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+        />
         <div id="map"></div>
       </main>
     );
